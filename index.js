0f418195ea2ce2ab6689428780a29e8cf26c1d5b
linhung0319 <linhung0319@gmail.com>
2024-04-01 22:17:03 +0800

    Create a new post

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-let posts = {"1": "post 1", "2": "post 2"};
+let posts = {};
 
 app.use(bodyParser.urlencoded({ extend: true }));
 
@@ -12,6 +12,17 @@ app.get("/", (req, res) => {
     res.render("index.ejs", {
         posts: posts,
     });
+})
+
+app.get("/create-post", (req, res) => {
+    res.render("create_post.ejs");
+})
+
+app.post("/create-post", (req, res) => {
+    const {title, content} = req.body;
+    const postId = Date.now().toString();
+    posts[postId] = {title, content};
+    res.redirect('/')
 })
 
 app.listen(port, () => {
